e28372d0442f4024675d5a6dafb02492c9ed48b6
Sourav Sarkar <souravsantipur9635@gmail.com>
2023-08-18 09:37:40 +0530

    ok

---
+++ barajiaku-mart-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Nav from './Components/Navbar';
+import AllRoutes from './Routes/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <br />
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
